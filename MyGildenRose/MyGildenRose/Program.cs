2a580ab655a5c6e10901871a7c6f56e76dd1346f
Octavio Diaz <diazoctavioitexico@users.noreply.github.com>
2020-04-15 16:41:25 -0500

    added test case scenario where you need no movements from dependencies such as Item class

--- MyGildenRose/MyGildenRose/Program.cs
+++ MyGildenRose/MyGildenRose/Program.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary2;
 
 namespace MyGildenRose
 {
+
+
     public class Program
     {
+
+
         IList<Item> Items;
         static void Main(string[] args)
         {
@@ -31,6 +36,8 @@ namespace MyGildenRose
                                           }
 
             };
+
+
 
             app.UpdateQuality();
 
--- MyGildenRose/UnitTests/UnitTest1.cs
+++ MyGildenRose/UnitTests/UnitTest1.cs
@@ -1,12 +1,53 @@
 using Xunit;
+using MyGildenRose;
+using System.Reflection;
+using System.Linq;
 
 namespace UnitTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CheckItemClassMovedStillWorks()
         {
+            // note :  testing a movement of a file inside the same asembly does not really require a test unless you move item to another assembly
+            var main = new Program();
+            Assert.NotNull(main);
+        }
+
+        /// <summary>
+        /// Checks the movement of file from a dependency assembly, so this indicates that that assembly needs an specific resource
+        /// May not apply that much moving a Item to a Item.cs file but if this is moved to another project the test will fail
+        /// </summary>
+        [Fact]
+        public void CheckMovementOfFileItemDependency()
+        {
+            // assembly where you need to get sure of having that resource
+
+            var resourceName = "Item";
+            var assemblyName = "MyGildenRose";
+
+            var assembly = Assembly.
+                GetExecutingAssembly().
+                GetReferencedAssemblies().
+                Select(Assembly.Load).
+                ToList().
+                Single(p => p.FullName.StartsWith(assemblyName));
+
+            var types = assembly.GetTypes();
+
+            var dependencyItemFound = false;
+
+            foreach (var type in types)
+            {
+                if (type.Name.Equals(resourceName))
+                {
+                    dependencyItemFound = true;
+                    break;
+                }
+            }
+
+            Assert.True(dependencyItemFound == true);
 
         }
     }
